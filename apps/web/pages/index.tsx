6058dc04c051d20101324a07bff41f1de5acb310
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-02-07 20:20:56 +0630

    chore: add styled tailwind css component

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,9 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div className="grid h-screen place-content-center">
-      <Button variant="primary" className="bg-red-500">
-        Hello world
-      </Button>
+      <Button>Primary</Button>
     </div>
   );
 }
--- packages/ui/components/button/Button.tsx
+++ packages/ui/components/button/Button.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren } from "react";
-import { twMerge } from "tailwind-merge";
+import tw from "tailwind-styled-components";
 
 interface IButtonProps
   extends PropsWithChildren<ComponentPropsWithoutRef<"button">> {
@@ -9,26 +9,43 @@ interface IButtonProps
   disabled?: boolean;
 }
 
+const StyledButton = tw.button<IButtonProps>`
+inline-flex items-center justify-center font-medium rounded-md focus:outline-none 
+focus:ring-2 focus:ring-offset-2 focus:ring-zinc-400
+
+${({ variant }) =>
+  variant === "primary"
+    ? "bg-zinc-800 text-white"
+    : "bg-zinc-300 text-zinc-800"}
+
+${({ size }) => {
+  switch (size) {
+    case "small":
+      return "px-3 py-1 text-sm";
+    case "medium":
+      return "px-4 py-2 text-base";
+    case "large":
+      return "px-6 py-3 text-lg";
+  }
+}}
+
+$({ disabled }) => (disabled ? "opacity-50 cursor-not-allowed" : "")}
+${({ className }) => className}
+`;
+
 export const Button = forwardRef<HTMLButtonElement, IButtonProps>(
   ({ children, variant, size, className, disabled, ...props }, ref) => {
     return (
-      <button
-        type="button"
+      <StyledButton
         ref={ref}
-        className={twMerge(
-          "px-4 py-2 rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-200 focus:ring-white",
-          variant === "primary" && "bg-zinc-800 text-white",
-          variant === "secondary" && "bg-zinc-100 text-zinc-800",
-          size === "small" && "text-sm",
-          size === "medium" && "text-base",
-          size === "large" && "text-lg",
-          disabled && "opacity-50 cursor-not-allowed",
-          className
-        )}
+        variant={variant}
+        size={size}
+        className={className}
+        disabled={disabled}
         {...props}
       >
         {children}
-      </button>
+      </StyledButton>
     );
   }
 );
